4ae50b6b7f31eb4a53c68cd19494d66e9f5f48ab
Damien Chapart <damienchapart@gmail.com>
2024-10-19 23:46:21 +0200

    update dependency and darkmode

--- src/components/organisms/navbar/Navbar.tsx
+++ src/components/organisms/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import NavButton from "components/atoms/navButton";
 import Logos from "components/atoms/logos";
 import { useEffect, useState } from "react";
@@ -37,19 +37,24 @@ const Navbar: React.FC<NavbarProps> = ({ page, toggleTheme }) => {
 
                     <div className="hidden lg:block">
                         <NavLink aria-label="About" to="/about"
-                              className="link-underline rounded-t py-1 px-2 text-gray-900 hover:bg-gray-200 sm:py-2 sm:px-3">about</NavLink>
+                            className="link-underline rounded-sm py-1 px-2 text-gray-900 hover:bg-gray-200 dark:hover:bg-blue-950 sm:py-2 sm:px-3 dark:text-white">about</NavLink>
                         <NavLink aria-label="Projects" to="/projects"
-                              className="link-underline rounded-t py-1 px-2 text-gray-900 hover:bg-gray-200 sm:py-2 sm:px-3">projects</NavLink>
+                            className="link-underline rounded-sm py-1 px-2 text-gray-900 hover:bg-gray-200 dark:hover:bg-blue-950 sm:py-2 sm:px-3 dark:text-white">projects</NavLink>
                         <NavLink aria-label="Contact" to="/contact"
-                              className="link-underline rounded-t py-1 px-2 text-gray-900 hover:bg-gray-200 sm:py-2 sm:px-3">contact</NavLink>
+                            className="link-underline rounded-sm py-1 px-2 text-gray-900 hover:bg-gray-200 dark:hover:bg-blue-950 sm:py-2 sm:px-3 dark:text-white">contact</NavLink>
                     </div>
                     <NavButton className="py-2"><Logos.Github
-                        className="w-6 h-6 group-hover:animate-spin transition-all duration-200 ease-in-out text-default dark:text-white "/></NavButton>
+                        className="w-6 h-6 group-hover:animate-spin transition-all duration-200 ease-in-out text-default dark:text-white " /></NavButton>
                     <NavButton className="py-2" onClick={onToggleTheme} >
-                        {theme === "light" ? <Logos.Moon className="w-6 h-6 group-hover transition-all duration-200 ease-in-out "/> : theme === "dark" ? <Logos.Sun className="w-6 h-6 group-hover transition-all duration-200 ease-in-out "/> : <Logos.Desktop className="w-6 h-6 group-hover transition-all duration-200 ease-in-out "/>}
+                        {theme === "light" ? <Logos.Moon className="w-6 h-6 group-hover transition-all duration-200 ease-in-out text-default dark:text-white" /> : theme === "dark" ? <Logos.Sun className="w-6 h-6 group-hover transition-all duration-200 ease-in-out text-default dark:text-white" /> : <Logos.Desktop className="w-6 h-6 group-hover transition-all duration-200 ease-in-out text-default dark:text-white" />}
                     </NavButton>
                     <NavButton className="py-2"><Logos.Menu
-                        className="w-6 h-6 group-hover transition-all duration-200 ease-in-out text-default dark:text-white "/></NavButton>
+                        className="w-6 h-6 group-hover transition-all duration-200 ease-in-out text-default dark:text-white " />
+                        <div className="fixed top-24 w-52 text-right">
+
+                        </div>
+                    </NavButton>
+                    
                 </div>
 
             </nav>
