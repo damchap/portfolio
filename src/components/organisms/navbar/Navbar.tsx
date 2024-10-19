8d9ce33e05bfd8b195f5f65d66f687eb7d13481a
Damien Chapart <damienchapart@gmail.com>
2024-10-19 23:18:34 +0200

    update dark mode

--- src/components/organisms/navbar/Navbar.tsx
+++ src/components/organisms/navbar/Navbar.tsx
@@ -37,19 +37,19 @@ const Navbar: React.FC<NavbarProps> = ({ page, toggleTheme }) => {
 
                     <div className="hidden lg:block">
                         <NavLink aria-label="About" to="/about"
-                              className="link-underline rounded-t py-1 px-2 text-gray-900 hover:bg-gray-200   sm:py-2 sm:px-3">about</NavLink>
+                              className="link-underline rounded-t py-1 px-2 text-gray-900 hover:bg-gray-200 sm:py-2 sm:px-3">about</NavLink>
                         <NavLink aria-label="Projects" to="/projects"
-                              className="link-underline rounded-t py-1 px-2 text-gray-900 hover:bg-gray-200   sm:py-2 sm:px-3">projects</NavLink>
+                              className="link-underline rounded-t py-1 px-2 text-gray-900 hover:bg-gray-200 sm:py-2 sm:px-3">projects</NavLink>
                         <NavLink aria-label="Contact" to="/contact"
-                              className="link-underline rounded-t py-1 px-2 text-gray-900 hover:bg-gray-200   sm:py-2 sm:px-3">contact</NavLink>
+                              className="link-underline rounded-t py-1 px-2 text-gray-900 hover:bg-gray-200 sm:py-2 sm:px-3">contact</NavLink>
                     </div>
                     <NavButton className="py-2"><Logos.Github
-                        className="w-6 h-6 group-hover:animate-spin transition-all duration-200 ease-in-out "/></NavButton>
+                        className="w-6 h-6 group-hover:animate-spin transition-all duration-200 ease-in-out text-default dark:text-white "/></NavButton>
                     <NavButton className="py-2" onClick={onToggleTheme} >
                         {theme === "light" ? <Logos.Moon className="w-6 h-6 group-hover transition-all duration-200 ease-in-out "/> : theme === "dark" ? <Logos.Sun className="w-6 h-6 group-hover transition-all duration-200 ease-in-out "/> : <Logos.Desktop className="w-6 h-6 group-hover transition-all duration-200 ease-in-out "/>}
                     </NavButton>
                     <NavButton className="py-2"><Logos.Menu
-                        className="w-6 h-6 group-hover transition-all duration-200 ease-in-out "/></NavButton>
+                        className="w-6 h-6 group-hover transition-all duration-200 ease-in-out text-default dark:text-white "/></NavButton>
                 </div>
 
             </nav>
